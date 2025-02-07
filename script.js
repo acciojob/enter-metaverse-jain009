6362854c0a0dabb269f8744ce6966227753e5ff7
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-07 19:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DomContenedLoaded", () => {
+	let statusElement = documnet.getEelmentById("statusE");
+	let enterButton = document.getElementById("enterBtn");
+
+	enterButton.addEventListner("Click", () => {
+		statusElement.innerHtml() = "<h1>Entered Metaverse</h1>"
+	});
+});
